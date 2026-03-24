a28792ce74303368f36e6ce33e89580de99ec0b7
maizer <ma1zeryu02@gmail.com>
2026-03-24 13:30:02 +0800

    day11

--- 11 - Custom Video Player/README.md
+++ 11 - Custom Video Player/README.md
@@ -50,3 +50,23 @@ video.addEventListener("timeupdate", () => {
 ## hook是什么
 
 在一个事后插入一段你的逻辑，比如click时间后执行的代码就是一个hook
+
+`const skipButtons = player.querySelectorAll('[data-skip]');`这样拿下来的是一个`NodeList`，想绑定事件需要forEach
+
+## 优化
+
+增加了一个直接随着拖动更改进度条的函数，避免一卡一卡的，但是我代码里的逻辑提炼的不是很好，有一些重复操作可以单独抽出来，懒了就不改了
+
+```javascript
+function updateProgress(e) {
+  const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+  const percent = (scrubTime / video.duration) * 100;
+  progressBar.style.flexBasis = `${percent}%`;
+}
+```
+
+防止用户鼠标离开进度条区域再松开，导致mousedown变量错误
+
+```javascript
+progress.addEventListener("mouseleave", () => (mousedown = false));
+```
--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -4,7 +4,7 @@ const video = player.querySelector('.viewer');
 const progress = player.querySelector('.progress');
 const progressBar = player.querySelector('.progress__filled');
 const toggle = player.querySelector('.toggle');
-const skipButtons = player.querySelectorAll('[data-skip]');
+const skipButtons = player.querySelectorAll('[data-skip]');//取的是一个NodeList
 const ranges = player.querySelectorAll('.player__slider');
 
 // Build out functions
@@ -21,8 +21,48 @@ function updateButton() {
     toggle.textContent = icon;
 }
 
+function skip() {
+    video.currentTime += parseFloat(this.dataset.skip);
+}
+
+function handleRangeUpdate() {
+    video[this.name] = this.value; //wooooooo
+}
+
+function handleProgress() {
+    const percent = (video.currentTime / video.duration) * 100;
+    progressBar.style.flexBasis = `${percent}%`;
+}
+
+function updateProgress(e) 
+{
+    const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+    const percent = (scrubTime / video.duration) * 100;
+    progressBar.style.flexBasis = `${percent}%`;
+}
+
+function scrub(e) {
+    const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+    video.currentTime = scrubTime;
+}
+let mousedown = false;
+
 // Hook up the event listens
 video.addEventListener('click', togglePlay);
 video.addEventListener('play', updateButton);
 video.addEventListener('pause', updateButton);
+video.addEventListener('timeupdate', handleProgress);
+
 toggle.addEventListener('click', togglePlay);
+skipButtons.forEach(skipButton => skipButton.addEventListener('click', skip));
+ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
+ranges.forEach(range => range.addEventListener('input', handleRangeUpdate));
+progress.addEventListener('click', scrub);
+progress.addEventListener('mousemove', (e) => {
+    if (!mousedown) return;
+    updateProgress(e);
+    scrub(e);
+});
+progress.addEventListener('mousedown', () => mousedown = true);
+progress.addEventListener('mouseup', () => mousedown = false);
+progress.addEventListener('mouseleave', () => mousedown = false);
