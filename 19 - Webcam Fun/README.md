62f454b83043de3e8a6deb78dceed5f7b0e96d95
maizer <ma1zeryu02@gmail.com>
2026-03-28 16:43:16 +0800

    day19

--- 19 - Webcam Fun/README.md
+++ 19 - Webcam Fun/README.md
@@ -3,3 +3,5 @@
 利用`npm install`和`npm start`来安装`package.json`跑起来前端项目，`http://localhost:3000`，因为有些浏览器api对权限有限制，必须用服务器跑起来才能调用一些api，
 
 普通资源，比如图片、文件`img.src = URL.createObjectURL(file);`，媒体流比如摄像头，麦克风流`video.srcObject = stream;`原视频的写法貌似已经被摒弃了，啊啊啊视频已经旧到原方法被浏览器移除支持了hh
+
+核心通过`let pixels = ctx.getImageData(0, 0, width, height);`获得图像的`RGBA`像素数组，`[R, G, B, A,  R, G, B, A,  R, G, B, A, ...]`然后对这些数据进行操作
--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -26,8 +26,10 @@ function paintToCanvas() {
         //take the pixels out
         let pixels = ctx.getImageData(0, 0, width, height);
         //mess with them
-        pixels = redEffect(pixels);
+        // pixels = rgbSplit(pixels);
+        // ctx.globalAlpha = 0.1;
         // put them back
+        pixels = greenScreen(pixels);
         ctx.putImageData(pixels, 0, 0);
     }, 16);
 }
@@ -44,11 +46,38 @@ function redEffect(pixels) {
 //25分42秒
 function rgbSplit(pixels) {
     for(let i = 0;i < pixels.data.length;i += 4) {
-        pixels.data[i] = pixels.data[i] + 100; //r
-        pixels.data[i + 1] = pixels.data[i + 1] - 50;//g
-        pixels.data[i + 2] = pixels.data[i + 2] * 0.5;//b
+        pixels.data[i - 150] = pixels.data[i]; //r
+        pixels.data[i + 100] = pixels.data[i + 1]//g
+        pixels.data[i - 550] = pixels.data[i + 2];//b
     }
     return pixels;
+}
+
+function greenScreen(pixels) {
+  const levels = {};
+
+  document.querySelectorAll('.rgb input').forEach((input) => {
+    levels[input.name] = input.value;
+  });
+
+  for (let i = 0; i < pixels.data.length; i = i + 4) {
+    red = pixels.data[i + 0];
+    green = pixels.data[i + 1];
+    blue = pixels.data[i + 2];
+    alpha = pixels.data[i + 3];
+
+    if (red >= levels.rmin
+      && green >= levels.gmin
+      && blue >= levels.bmin
+      && red <= levels.rmax
+      && green <= levels.gmax
+      && blue <= levels.bmax) {
+      // take it out!
+      pixels.data[i + 3] = 0;
+    }
+  }
+
+  return pixels;
 }
 
 function takePhoto() {
